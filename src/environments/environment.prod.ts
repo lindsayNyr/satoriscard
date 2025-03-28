af5308dda1a045faed3bdeb31bec6dc2eb0f65b4
Lindsay <lindsay.noyer@icloud.com>
2025-03-28 21:47:28 +0100

    add : environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+    production: true, 
+    api: {
+        protocol: 'https',
+        host: 'randomuser.me',
+        endpoints: {
+            user: '/api/?results=1',
+            users: 'api/?results=10'
+        }
+    }
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+    production: false, 
+    api: {
+        protocol: 'https',
+        host: 'randomuser.me',
+        endpoints: {
+            user: '/api/?results=1',
+            users: 'api/?results=10'
+        }
+    }
+
+};
